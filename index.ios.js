fa0a672cd92394a99aa3530267dc36df8f4b0dac
曹巍 <tsaowe@gmail.com>
2016-04-13 17:36:30 +0800

    2016年04月13日17:36:28

--- index.ios.js
+++ index.ios.js
@@ -3,52 +3,45 @@
  * https://github.com/facebook/react-native
  */
 
- var ReactNative = require('react-native');
+'use strict';
+var React = require('react-native');
+var {
+    AppRegistry,
+    StyleSheet,
+    Text,
+    View,
+    Image
+    } = React;
 
-import React, {
-  AppRegistry,
-  Component,
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
 
-class RCTJumei extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
-      </View>
-    );
-  }
-}
+var styles = StyleSheet.create({
+    container:{
+        flex:1,
+        justifyContent:'center',
+        alignItems:'center',
+        backgroundColor:'#05a5d1'
+    } ,
+    welcome:{
+        fontSize:20,
+        color:'#fff'
+    }
+});
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+var RCTJumei = React.createClass({
+    render:function(){
+        return (
+            <View style={styles.container}>
+                <Text style={styles.welcome}>
+                    欢迎来到曹蛋蛋的世界
+                </Text>
+                <Image
+                    style={{width:50,height:50,resizeMode:Image.resizeMode.contain}}
+                    source={{uri:'https://facebook.github.io/react-native/img/header_logo.png'}}
+                >
+                </Image>
+            </View>
+        )
+    }
 });
 
 AppRegistry.registerComponent('RCTJumei', () => RCTJumei);
