c907748d405f340c53d0041723a4c7514f4b3efc
曹巍 <tsaowe@gmail.com>
2016-04-15 15:30:45 +0800

    2016年04月15日15:30:44

--- index.ios.js
+++ index.ios.js
@@ -4,65 +4,92 @@
  */
 
 var React = require('react-native');
-var Header = require('./header');
-
 var {
-    AppRegistry,
     StyleSheet,
+    Text,
+    AppRegistry,
     View,
-    Text
+    Image,
+    TouchableOpacity,
     } = React;
 
-var List = React.createClass({
-    render: function(){
-        return (
-            <View style={styles.list_item}>
-                <Text style={styles.list_item_font}>{this.props.title}</Text>
-            </View>
-        );
-    }
-});
-
-var ImportantNews = React.createClass({
-    show: function(title){
-        alert(title);
+var imgs = [
+    "http://p3.jmstatic.com/product/000/640/640073_std/640073_160_160.jpg",
+    "http://p1.jmstatic.com/product/000/013/13051_std/13051_160_160.jpg",
+    "http://p2.jmstatic.com/product/001/951/1951962_std/1951962_160_160.jpg",
+    "http://p0.jmstatic.com/product/000/185/185540_std/185540_160_160.jpg",
+    "http://p2.jmstatic.com/product/000/777/777257_std/777257_160_160.jpg",
+    "http://p3.jmstatic.com/product/000/670/670323_std/670323_160_160.jpg",
+    "http://p3.jmstatic.com/product/001/353/1353468_std/1353468_160_160.jpg",
+    "http://p1.jmstatic.com/product/000/230/230856_std/230856_160_160.jpg",
+    "http://p3.jmstatic.com/product/000/231/231258_std/231258_160_160.jpg",
+    "http://p2.jmstatic.com/product/000/017/17542_std/17542_160_160.jpg",
+    "http://p3.jmstatic.com/product/000/015/15228_std/15228_160_160.jpg",
+    "http://p0.jmstatic.com/product/001/095/1095430_std/1095430_160_160.jpg",
+    "http://p0.jmstatic.com/product/000/006/6935_std/6935_160_160.jpg",
+    "http://p1.jmstatic.com/product/001/602/1602226_std/1602226_160_160.jpg",
+    "http://p3.jmstatic.com/product/000/009/9878_std/9878_160_160.jpg",
+    "http://p1.jmstatic.com/product/000/923/923266_std/923266_160_160.jpg",
+    "http://p3.jmstatic.com/product/000/980/980263_std/980263_160_160.jpg",
+    "http://p0.jmstatic.com/product/000/006/6935_std/6935_160_160.jpg"
+];
+var MyImage = React.createClass({
+    getInitialState: function(){
+        var imgs = this.props.imgs;
+        return {
+            imgs: imgs,
+            count: 0
+        };
+    },
+    goNext: function(){
+        var count = this.state.count;
+        count ++;
+        if(count < imgs.length){
+            this.setState({
+                count: count
+            });
+        }
+    },
+    goPreview: function(){
+        var count = this.state.count;
+        count --;
+        if(count >= 0){
+            this.setState({
+                count: count
+            });
+        }
     },
     render: function(){
-        var news = this.props.news.map((item)=>{
-            return   (
-                <Text
-                    onPress={this.show.bind(this,item)}
-                    numberOfLines={2}
-                    style={styles.news_item}>
-                        {item}
-                </Text>
-            );
-        });
-        return (
-            <View style={styles.flex}>
-                <Text style={styles.news_title}>今日要闻</Text>
-                {news}
+        return(
+            <View style={[styles.flex]}>
+                <View style={styles.image}>
+                    <Image style={styles.img}
+                        source={{uri: this.state.imgs[this.state.count]}}
+                        resizeMode="contain"
+                    />
+                </View>
+                <View style={styles.btns}>
+                    <TouchableOpacity onPress={this.goPreview}>
+                        <View style={styles.btn}>
+                            <Text>上一张</Text>
+                        </View>
+                    </TouchableOpacity>
+                    <TouchableOpacity onPress={this.goNext}>
+                        <View style={styles.btn}>
+                            <Text>下一张</Text>
+                        </View>
+                    </TouchableOpacity>
+                </View>
             </View>
         );
     }
 });
 
-var app = React.createClass({
+var App = React.createClass({
     render: function(){
-        var news = [
-            '1、刘慈欣《三体》获“雨果奖”为中国作家首次',
-            '2、京津冀协同发展定位明确：北京无经济中心表述',
-            '3、好奇宝宝第一次淋雨，父亲用镜头记录了下来',
-            '4、京津冀协同发展定位明确：:北京无经济中心表述+好奇宝宝第一次淋雨，父亲用镜头记录了下来'
-        ];
-        return (
-            <View style={styles.flex}>
-                <Header></Header>
-                <List title='宇航员在太空宣布“三体”获奖'></List>
-                <List title='NASA发短片纪念火星征程50年'></List>
-                <List title='男生连续做一周苦瓜吃吐女友'></List>
-                <List title='女童遭鲨鱼袭击又下海救伙伴'></List>
-                <ImportantNews news={news}></ImportantNews>
+        return(
+            <View style={[styles.flex, {marginTop:40}]}>
+                <MyImage imgs={imgs}></MyImage>
             </View>
         );
     }
@@ -70,32 +97,36 @@ var app = React.createClass({
 
 var styles = StyleSheet.create({
     flex:{
-        flex:1
+        flex: 1,
+        alignItems:'center'
     },
-    list_item:{
-        height:40,
-        marginLeft:10,
-        marginRight:10,
-        borderBottomWidth:1,
-        borderBottomColor: '#ddd',
-        justifyContent: 'center'
+    image:{
+        borderWidth:1,
+        width:300,
+        height:199,
+        borderRadius:5,
+        borderColor:'#ccc',
+        marginBottom:5
     },
-    list_item_font:{
-        fontSize:16
+    img:{
+        height:201,
+        width:300,
+        paddingBottom:5
     },
-    news_title:{
-        fontSize:20,
-        fontWeight:'bold',
-        color: '#CD1D1C',
-        marginLeft:10,
-        marginTop:15
+    btns:{
+        flexDirection: 'row',
+        justifyContent: 'center',
+        marginTop:20
     },
-    news_item:{
-        marginLeft:10,
-        marginRight:10,
-        fontSize:15,
-        lineHeight:20
-    }
+    btn:{
+        width:60,
+        height:30,
+        borderColor: '#0089FF',
+        borderWidth: 1,
+        justifyContent: 'center',
+        alignItems:'center',
+        borderRadius:3,
+        marginRight:20,
+    },
 });
-
-AppRegistry.registerComponent('RCTJumei', () => app);
+AppRegistry.registerComponent('RCTJumei', () => App);
