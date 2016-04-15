230d9afc2ebb697bdc8278d5ce1ecb5997392b6e
曹巍 <tsaowe@gmail.com>
2016-04-15 10:08:03 +0800

    2016年04月15日10:08:02

--- index.ios.js
+++ index.ios.js
@@ -3,44 +3,101 @@
  * https://github.com/facebook/react-native
  */
 
-'use strict';
 var React = require('react-native');
+var Header = require('./header');
+
 var {
     AppRegistry,
     StyleSheet,
-    Text,
     View,
-    Image
+    Text
     } = React;
 
+var List = React.createClass({
+    render: function(){
+        return (
+            <View style={styles.list_item}>
+                <Text style={styles.list_item_font}>{this.props.title}</Text>
+            </View>
+        );
+    }
+});
+
+var ImportantNews = React.createClass({
+    show: function(title){
+        alert(title);
+    },
+    render: function(){
+        var news = [];
+        for(var i in this.props.news){
+            var text = (
+                <Text
+                    onPress={this.show.bind(this, this.props.news[i])}
+                    numberOfLines={2}
+                    style={styles.news_item}>
+          {this.props.news[i]}
+                </Text>
+            );
+            news.push(text);
+        }
+        return (
+            <View style={styles.flex}>
+                <Text style={styles.news_title}>今日要闻</Text>
+        {news}
+            </View>
+        );
+    }
+});
+
+var app = React.createClass({
+    render: function(){
+        var news = [
+            '1、刘慈欣《三体》获“雨果奖”为中国作家首次',
+            '2、京津冀协同发展定位明确：北京无经济中心表述',
+            '3、好奇宝宝第一次淋雨，父亲用镜头记录了下来',
+            '4、京津冀协同发展定位明确：:北京无经济中心表述+好奇宝宝第一次淋雨，父亲用镜头记录了下来'
+        ];
+        return (
+            <View style={styles.flex}>
+                <Header></Header>
+                <List title='宇航员在太空宣布“三体”获奖'></List>
+                <List title='NASA发短片纪念火星征程50年'></List>
+                <List title='男生连续做一周苦瓜吃吐女友'></List>
+                <List title='女童遭鲨鱼袭击又下海救伙伴'></List>
+                <ImportantNews news={news}></ImportantNews>
+            </View>
+        );
+    }
+});
 
 var styles = StyleSheet.create({
-    container:{
-        flex:1,
-        justifyContent:'center',
-        alignItems:'center',
-        backgroundColor:'#05a5d1'
-    } ,
-    welcome:{
+    flex:{
+        flex:1
+    },
+    list_item:{
+        height:40,
+        marginLeft:10,
+        marginRight:10,
+        borderBottomWidth:1,
+        borderBottomColor: '#ddd',
+        justifyContent: 'center'
+    },
+    list_item_font:{
+        fontSize:16
+    },
+    news_title:{
         fontSize:20,
-        color:'#fff'
+        fontWeight:'bold',
+        color: '#CD1D1C',
+        marginLeft:10,
+        marginTop:15
+    },
+    news_item:{
+        marginLeft:10,
+        marginRight:10,
+        fontSize:15,
+        lineHeight:20
     }
 });
 
-var RCTJumei = React.createClass({
-    render:function(){
-        return (
-            <View style={styles.container}>
-                <Text style={styles.welcome}>
-                    欢迎来到全新的native APP世界
-                </Text>
-                <Image
-                    style={{width:50,height:50,resizeMode:Image.resizeMode.contain}}
-                    source={{uri:'https://facebook.github.io/react-native/img/header_logo.png'}}
-                />
-            </View>
-        )
-    }
-});
-
-AppRegistry.registerComponent('RCTJumei', () => RCTJumei);
+AppRegistry.registerComponent('RCTJumei', () => app);
