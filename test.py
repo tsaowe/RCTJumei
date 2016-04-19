0e0d047d23daff23be5a0ea1035db0684319d61c
曹巍 <tsaowe@gmail.com>
2016-04-19 11:38:03 +0800

    2016年04月19日11:38:02

---
+++ test.py
@@ -0,0 +1,21 @@
+import commands
+import re
+import codecs
+
+if __name__ == '__main__':
+    status, output = commands.getstatusoutput("ifconfig|grep netmask|grep -v 127.0.0.1")
+    pat = re.compile(r'(?<![\.\d])(?:\d{1,3}\.){3}\d{1,3}(?![\.\d])')
+    ips = pat.findall(output)
+    currentip = ips[0]
+    file_object = open('ios/RCTJumei/AppDelegate.m')
+    try:
+         all_the_text = file_object.read()
+         searchIps = pat.findall(all_the_text)
+         searchIp = searchIps[0]
+         all_the_text = all_the_text.replace(searchIp, currentip)
+         # print all_the_text
+         f = codecs.open('ios/RCTJumei/AppDelegate.m','w','utf-8')
+         f.write(all_the_text)
+         f.close()
+    finally:
+         file_object.close( )
