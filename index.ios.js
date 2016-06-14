4d1db5979cdd78afa14f7960f3d399af9b6fff38
曹巍 <tsaowe@gmail.com>
2016-06-14 13:45:16 +0800

    simplify index.ios.js

--- index.ios.js
+++ index.ios.js
@@ -1,16 +1,8 @@
 var React = require('react-native');
 var {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View,
-  StatusBar,
-  Animated,
-  Image,
-  TouchableOpacity
+  AppRegistry
 } = React;
 
-import custumeStyle from './style/index';
 import Welcome from './components/welcome';
 
 AppRegistry.registerComponent('RCTJumei', () => Welcome);
