e52960978e1d8b332df232961abd86ba0c8281cf
曹巍 <tsaowe@gmail.com>
2016-06-15 22:40:46 +0800

    2016年06月15日22:40:45

--- components/parts/tab1/navigator.js
+++ components/parts/tab1/navigator.js
@@ -3,12 +3,31 @@
  */
 var React = require('react');
 var ReactNative = require('react-native');
-let {ScrollView, Text, StyleSheet, View, TouchableOpacity, Alert} = ReactNative;
+let {ScrollView, Text, StyleSheet, View, TouchableOpacity, Alert,Dimensions} = ReactNative;
 
 import list from './navigatorData';
 
 
 import colors from '../../common/color';
+
+let {height, width} = Dimensions.get('window');
+
+
+const generateX = (windowWidth,widths,Xs,index)=>{
+    let maxX = widths.reduce((a, b)=>(a + b)) - windowWidth;
+    let minX = 0;
+    let currentElementCenterX = Xs[index] + widths[index] / 2;
+    let elementScrollX = currentElementCenterX - windowWidth / 2;
+    let left = elementScrollX;
+    if(elementScrollX < minX){
+        left = 0;    
+    }
+    if(elementScrollX > maxX){
+        left = maxX;
+    }
+    return left;
+};
+
 
 const styles = StyleSheet.create({
     scrollView: {
@@ -18,12 +37,21 @@ const styles = StyleSheet.create({
     },
     text: {
         fontSize: 15,
-        color: '#838383'
+        color: '#838383',
+        height:22 - 1 / ReactNative.PixelRatio.get()
 
     },
     innerView: {
         paddingLeft: 10,
-        paddingRight: 10
+        paddingRight: 10,
+        height:22
+    },
+    touchable:{
+        height:22
+    },
+    touchableHighLight:{
+        borderBottomWidth:1,
+        borderBottomColor:colors.JMRed
     }
 });
 
@@ -40,22 +68,12 @@ var Component = React.createClass({
         this.setState({
             active: index
         });
-        let all = this.state.width.reduce((a, b)=>(a + b));
-        let left = this.state.width.filter(function (value, i) {
-            return i < index;
-        });
-        if (left.length) {
-            left = left.reduce(function (a = 0, b = 0) {
-                return a + b;
-            });
-        } else {
-            left = 0;
-        }
-        setTimeout(()=> {
+        
+        setTimeout(function () {
             this.refs.scrollView.scrollTo({
-                x: left
+                x: generateX(width,this.state.width,this.state.x,index)
             });
-        }, 10);
+        }.bind(this), 10);
     }
     ,
     render: function () {
@@ -64,6 +82,7 @@ var Component = React.createClass({
                 ref="scrollView"
                 automaticallyAdjustContentInsets={false}
                 showsHorizontalScrollIndicator={false}
+                showsVerticalScrollIndicator={false}
                 horizontal={true}
                 style={[styles.scrollView]}>
                 {list.map(function (text, i) {
@@ -73,16 +92,18 @@ var Component = React.createClass({
                             key={i}
                             onPress={this.setActive.bind(this,i)}
                             onLayout={(event) => {
-                             let {x, y, width, height} = event.nativeEvent.layout;
-                             this.setState(function(state) {
-                                    state.width[i] = width;
-                                    state.x[i] = x;
-                                    return state;
-                                });
-                             }}>
-                            <View style={styles.innerView}>
-                                <Text
-                                    style={[styles.text,((i === this.state.active )? {color : colors.JMRed,transform: [{'scale':1.02}]} : {})]}>{text}</Text>
+                                 let {x, y, width, height} = event.nativeEvent.layout;
+                                 this.setState(function(state) {
+                                        state.width[i] = width;
+                                        state.x[i] = x;
+                                    });
+                             }}
+                            style={[styles.touchable]}>
+                            <View style={[styles.innerView]}>
+                                <View style={(i === this.state.active ) ? styles.touchableHighLight:{}}>
+                                    <Text
+                                        style={[styles.text,((i === this.state.active )? {color : colors.JMRed,transform: [{'scale':1.02}]} : {})]}>{text}</Text>
+                                </View>
                             </View>
                         </TouchableOpacity>
                     );
